57b97ff69d23a1d70b4f1ff45576f25c297da69e
peacemaker19881 <jeandelapaix2013@gmail.com>
2025-02-16 11:54:03 +0200

    mycommit after creating bumba web project

---
+++ src/components/reusablebutton_component.vue
@@ -0,0 +1,31 @@
+<template>
+    <button :class="buttonClass" @click="handleClick">
+      <slot></slot>
+    </button>
+  </template>
+  <script>
+  export default {
+    name: 'reusablebutton_component',
+    props: {
+      buttonClass: {
+        type: String,
+        default: 'default-button'
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit('click');
+      }
+    }
+  }
+  </script>
+
+  <style scoped>
+  .default-button {
+    background-color: blue;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+  }
+  </style>
---
+++ src/components/studentregistartion_component.vue
@@ -0,0 +1,14 @@
+<template>
+ <h1>Welcome in gs Bumba TSS</h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'studentregistartion_component',
+  components: {
+  
+  }
+}
+</script>
